59ea70c54397621b83eefba71cb1a0475ba344fe
Jonathan Jacobs <jonathan.00@live.co.uk>
2025-07-15 14:36:57 +0100

    Validate ShortenRequest fields. Fix issue where links in UI were not linking to expected page.

--- src/main/java/com/jonathanj00/url_shortener/dto/ShortenRequest.java
+++ src/main/java/com/jonathanj00/url_shortener/dto/ShortenRequest.java
@@ -1,10 +1,18 @@
 package com.jonathanj00.url_shortener.dto;
 
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import org.hibernate.validator.constraints.URL;
+
 /**
  * DTO class defining request body of requests to /shorten interface method.
  */
 public class ShortenRequest {
+    @URL(message = "Invalid URL format")
+    @NotNull
     private String fullUrl;
+
+    @Pattern(regexp = "^[A-Za-z0-9]*$")
     private String customAlias;
 
     public String getFullUrl() {
